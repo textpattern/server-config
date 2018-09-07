927bfddbe0fc7c7e0fe67af787a81b624e9bd996
Pete Cooper <pete@pragmatika.net>
2018-09-07 16:05:05 +0100

    roll out automation to classic design patterns
    Signed-off-by: Pete Cooper <pete@pragmatika.net>

---
+++ servers/files/tractor.textpattern.com/var/www/vhosts/textpattern.com/design-patterns-classic-4-6/staging/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+### design-patterns-classic-4-6.textpattern.com build script
+
+### get latest files
+rm -rf /var/www/vhosts/textpattern.com/design-patterns-classic-4-6/staging/_github
+git clone --branch '4.6.x' --depth 1 --single-branch https://github.com/philwareham/textpattern-classic-admin-theme.git /var/www/vhosts/textpattern.com/design-patterns-classic-4-6/staging/_github
+
+### drop it (in) like it's hot
+rm -rf /var/www/vhosts/textpattern.com/design-patterns-classic-4-6/live/*
+mv /var/www/vhosts/textpattern.com/design-patterns-classic-4-6/staging/_github/dist /var/www/vhosts/textpattern.com/design-patterns-classic-4-6/live/
+mv /var/www/vhosts/textpattern.com/design-patterns-classic-4-6/staging/_github/docs /var/www/vhosts/textpattern.com/design-patterns-classic-4-6/live/
+mv /var/www/vhosts/textpattern.com/design-patterns-classic-4-6/staging/_github/mockups /var/www/vhosts/textpattern.com/design-patterns-classic-4-6/live/
+
+### mop up
+chown -R www-data:www-data /var/www/vhosts/textpattern.com/design-patterns-classic-4-6/live/
+chmod -R 755 /var/www/vhosts/textpattern.com/design-patterns-classic-4-6/live/
+
+### done
+exit
