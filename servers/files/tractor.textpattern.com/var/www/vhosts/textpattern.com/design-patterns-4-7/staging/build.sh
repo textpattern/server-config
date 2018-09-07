d4adb3bee8a13ef3a91c34bb8deb9c59ef269454
Pete Cooper <pete@pragmatika.net>
2018-09-07 15:28:23 +0100

    two more build scripts
    Signed-off-by: Pete Cooper <pete@pragmatika.net>

---
+++ servers/files/tractor.textpattern.com/var/www/vhosts/textpattern.com/design-patterns-4-7/staging/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+### design-patterns-4-7.textpattern.com build script
+
+### get latest files
+rm -rf /var/www/vhosts/textpattern.com/design-patterns-4-7/staging/_github
+git clone --branch 'master' --depth 1 --single-branch https://github.com/philwareham/textpattern-hive-admin-theme.git /var/www/vhosts/textpattern.com/design-patterns-4-7/staging/_github
+
+### drop it (in) like it's hot
+rm -rf /var/www/vhosts/textpattern.com/design-patterns-4-7/live/*
+mv /var/www/vhosts/textpattern.com/design-patterns-4-7/staging/_github/dist /var/www/vhosts/textpattern.com/design-patterns-4-7/live/
+mv /var/www/vhosts/textpattern.com/design-patterns-4-7/staging/_github/docs /var/www/vhosts/textpattern.com/design-patterns-4-7/live/
+mv /var/www/vhosts/textpattern.com/design-patterns-4-7/staging/_github/mockups /var/www/vhosts/textpattern.com/design-patterns-4-7/live/
+
+### mop up
+chown -R www-data:www-data /var/www/vhosts/textpattern.com/design-patterns-4-7/live/
+chmod -R 755 /var/www/vhosts/textpattern.com/design-patterns-4-7/live/
+
+### done
+exit
