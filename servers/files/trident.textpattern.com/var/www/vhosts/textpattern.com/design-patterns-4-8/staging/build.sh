d78820fd80be87c7041e15ae1bf55f2bcdf0a215
Pete Cooper <pete@pragmatika.net>
2019-02-19 13:24:32 +0000

    start tracking 4.8 design patterns staging

---
+++ servers/files/trident.textpattern.com/var/www/vhosts/textpattern.com/design-patterns-4-8/staging/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+### design-patterns-4-8.textpattern.com build script
+
+### get latest files
+rm -rf /var/www/vhosts/textpattern.com/design-patterns-4-8/staging/_github
+git clone --branch 'master' --depth 1 --single-branch https://github.com/philwareham/textpattern-hive-admin-theme.git /var/www/vhosts/textpattern.com/design-patterns-4-8/staging/_github
+
+### drop it (in) like it's hot
+rm -rf /var/www/vhosts/textpattern.com/design-patterns-4-8/live/*
+mv /var/www/vhosts/textpattern.com/design-patterns-4-8/staging/_github/dist /var/www/vhosts/textpattern.com/design-patterns-4-8/live/
+mv /var/www/vhosts/textpattern.com/design-patterns-4-8/staging/_github/docs /var/www/vhosts/textpattern.com/design-patterns-4-8/live/
+mv /var/www/vhosts/textpattern.com/design-patterns-4-8/staging/_github/mockups /var/www/vhosts/textpattern.com/design-patterns-4-8/live/
+
+### mop up
+chown -R www-data:www-data /var/www/vhosts/textpattern.com/design-patterns-4-8/live/
+chmod -R 755 /var/www/vhosts/textpattern.com/design-patterns-4-8/live/
+rm -rf /var/www/vhosts/textpattern.com/design-patterns-4-8/staging/_github
+
+### done
+exit
