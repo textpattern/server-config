9a7be569d1ed5bc5d4aa65af6fbb7ef48a3dd35e
Pete Cooper <pete@pragmatika.net>
2020-06-22 12:45:53 +0100

    actually use the right version number

--- servers/files/trident.textpattern.com/var/www/vhosts/textpattern.com/design-patterns-4-9/staging/build.sh
+++ servers/files/trident.textpattern.com/var/www/vhosts/textpattern.com/design-patterns-4-9/staging/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-### design-patterns-4-8.textpattern.com build script
+### design-patterns-4-9.textpattern.com build script
 
 ### get latest files
-rm -rf /var/www/vhosts/textpattern.com/design-patterns-4-8/staging/_github
-git clone --branch 'master' --depth 1 --single-branch https://github.com/philwareham/textpattern-hive-admin-theme.git /var/www/vhosts/textpattern.com/design-patterns-4-8/staging/_github
+rm -rf /var/www/vhosts/textpattern.com/design-patterns-4-9/staging/_github
+git clone --branch 'master' --depth 1 --single-branch https://github.com/philwareham/textpattern-hive-admin-theme.git /var/www/vhosts/textpattern.com/design-patterns-4-9/staging/_github
 
 ### drop it (in) like it's hot
-rm -rf /var/www/vhosts/textpattern.com/design-patterns-4-8/live/*
-mv /var/www/vhosts/textpattern.com/design-patterns-4-8/staging/_github/dist /var/www/vhosts/textpattern.com/design-patterns-4-8/live/
-mv /var/www/vhosts/textpattern.com/design-patterns-4-8/staging/_github/docs /var/www/vhosts/textpattern.com/design-patterns-4-8/live/
-mv /var/www/vhosts/textpattern.com/design-patterns-4-8/staging/_github/mockups /var/www/vhosts/textpattern.com/design-patterns-4-8/live/
+rm -rf /var/www/vhosts/textpattern.com/design-patterns-4-9/live/*
+mv /var/www/vhosts/textpattern.com/design-patterns-4-9/staging/_github/dist /var/www/vhosts/textpattern.com/design-patterns-4-9/live/
+mv /var/www/vhosts/textpattern.com/design-patterns-4-9/staging/_github/docs /var/www/vhosts/textpattern.com/design-patterns-4-9/live/
+mv /var/www/vhosts/textpattern.com/design-patterns-4-9/staging/_github/mockups /var/www/vhosts/textpattern.com/design-patterns-4-9/live/
 
 ### mop up
-chown -R www-data:www-data /var/www/vhosts/textpattern.com/design-patterns-4-8/live/
-chmod -R 755 /var/www/vhosts/textpattern.com/design-patterns-4-8/live/
-rm -rf /var/www/vhosts/textpattern.com/design-patterns-4-8/staging/_github
+chown -R www-data:www-data /var/www/vhosts/textpattern.com/design-patterns-4-9/live/
+chmod -R 755 /var/www/vhosts/textpattern.com/design-patterns-4-9/live/
+rm -rf /var/www/vhosts/textpattern.com/design-patterns-4-9/staging/_github
 
 ### done
 exit
