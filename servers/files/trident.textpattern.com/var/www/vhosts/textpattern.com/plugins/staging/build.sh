3c669f2690235f213f80b20f7b40ebeccfc903f2
Pete Cooper <pete@pragmatika.net>
2020-06-29 18:11:02 +0100

    check for existence of README.md before repaving
    see https://github.com/textpattern/textpattern-curated-plugins-list/issues/6 and https://github.com/textpattern/server-config/issues/54

--- servers/files/trident.textpattern.com/var/www/vhosts/textpattern.com/plugins/staging/build.sh
+++ servers/files/trident.textpattern.com/var/www/vhosts/textpattern.com/plugins/staging/build.sh
@@ -6,11 +6,10 @@ rm -rf /var/www/vhosts/textpattern.com/plugins/staging/_github
 git clone --branch 'master' --depth 1 --single-branch https://github.com/textpattern/textpattern-curated-plugins-list.git /var/www/vhosts/textpattern.com/plugins/staging/_github
 
 ### drop it (in) like it's hot
-rm -rf /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/
-mkdir -p /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/
-mv /var/www/vhosts/textpattern.com/plugins/staging/_github/library-of-plugins/* /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/
-
-### mop up
-chown -R www-data:www-data /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/
-chmod -R 775 /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/
-rm -rf /var/www/vhosts/textpattern.com/plugins/staging/_github
+if [ -f /var/www/vhosts/textpattern.com/plugins/staging/_github/README.md ]; then
+rm -rf /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/ \
+&& mkdir -p /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/ \
+&& mv /var/www/vhosts/textpattern.com/plugins/staging/_github/library-of-plugins/* /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/ \
+&& chown -R www-data:www-data /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/ \
+&& chmod -R 775 /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/
+fi
