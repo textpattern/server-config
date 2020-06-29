be4397b8fded2d710bd042951feefc334f4f3981
Pete Cooper <pete@pragmatika.net>
2020-06-29 15:15:58 +0100

    use preferred url stub
    see https://github.com/textpattern/textpattern-curated-plugins-list/issues/6

--- servers/files/trident.textpattern.com/var/www/vhosts/textpattern.com/plugins/staging/build.sh
+++ servers/files/trident.textpattern.com/var/www/vhosts/textpattern.com/plugins/staging/build.sh
@@ -3,16 +3,16 @@
 
 ### get latest files
 rm -rf /var/www/vhosts/textpattern.com/plugins/staging/_github
-git clone --branch 'master' --depth 1 --single-branch https://github.com/textpattern/textpattern-curated-plugins-list.git /var/www/vhosts/textpattern.com/plugins/staging/_github
+git clone --branch 'master' --depth 1 --single-branch https://github.com/textpattern/textpattern-library-of-plugins.git /var/www/vhosts/textpattern.com/plugins/staging/_github
 
 ### drop it (in) like it's hot
-rm -rf /var/www/vhosts/textpattern.com/plugins/live/curated-plugins-list/
-mkdir -p /var/www/vhosts/textpattern.com/plugins/live/curated-plugins-list/
-mv /var/www/vhosts/textpattern.com/plugins/staging/_github/library-of-plugins/* /var/www/vhosts/textpattern.com/plugins/live/curated-plugins-list/
+rm -rf /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/
+mkdir -p /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/
+mv /var/www/vhosts/textpattern.com/plugins/staging/_github/library-of-plugins/* /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/
 
 ### mop up
-chown -R www-data:www-data /var/www/vhosts/textpattern.com/plugins/live/curated-plugins-list/
-chmod -R 775 /var/www/vhosts/textpattern.com/plugins/live/curated-plugins-list/
+chown -R www-data:www-data /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/
+chmod -R 775 /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/
 rm -rf /var/www/vhosts/textpattern.com/plugins/staging/_github
 
 ### done
