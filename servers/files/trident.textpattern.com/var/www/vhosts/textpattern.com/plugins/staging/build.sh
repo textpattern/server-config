aa1356f0d6d9e0536832aaf20bdcb72d35e7cc40
Pete Cooper <pete@pragmatika.net>
2020-06-29 14:32:31 +0100

    first pass at plugins build script
    See https://github.com/textpattern/textpattern-curated-plugins-list/issues/6

---
+++ servers/files/trident.textpattern.com/var/www/vhosts/textpattern.com/plugins/staging/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+### plugins.textpattern.com build script
+
+### get latest files
+rm -rf /var/www/vhosts/textpattern.com/plugins/staging/_github
+git clone --branch 'master' --depth 1 --single-branch https://github.com/textpattern/textpattern-curated-plugins-list.git /var/www/vhosts/textpattern.com/plugins/staging/_github
+
+### drop it (in) like it's hot
+rm -rf /var/www/vhosts/textpattern.com/plugins/live/curated-plugins-list/
+mkdir -p /var/www/vhosts/textpattern.com/plugins/live/curated-plugins-list/
+mv /var/www/vhosts/textpattern.com/plugins/staging/_github/* /var/www/vhosts/textpattern.com/plugins/live/curated-plugins-list/
+
+### mop up
+chown -R www-data:www-data /var/www/vhosts/textpattern.com/plugins/live/curated-plugins-list/
+chmod -R 775 /var/www/vhosts/textpattern.com/plugins/live/curated-plugins-list/
+rm -rf /var/www/vhosts/textpattern.com/plugins/staging/_github
+
+### done
+exit
