f9541692b00fb8729b386b46fd0ba73d3c5e712f
Pete Cooper <pete@pragmatika.net>
2020-06-29 15:20:17 +0100

    whoops

--- servers/files/trident.textpattern.com/var/www/vhosts/textpattern.com/plugins/staging/build.sh
+++ servers/files/trident.textpattern.com/var/www/vhosts/textpattern.com/plugins/staging/build.sh
@@ -3,7 +3,7 @@
 
 ### get latest files
 rm -rf /var/www/vhosts/textpattern.com/plugins/staging/_github
-git clone --branch 'master' --depth 1 --single-branch https://github.com/textpattern/textpattern-library-of-plugins.git /var/www/vhosts/textpattern.com/plugins/staging/_github
+git clone --branch 'master' --depth 1 --single-branch https://github.com/textpattern/textpattern-curated-plugins-list/git /var/www/vhosts/textpattern.com/plugins/staging/_github
 
 ### drop it (in) like it's hot
 rm -rf /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/
