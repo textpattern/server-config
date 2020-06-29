98290f2dfca062862b19b66e7bddb956f337a3d2
Pete Cooper <pete@pragmatika.net>
2020-06-29 18:58:28 +0100

    first pass at vars in plugin build script
    see https://github.com/textpattern/server-config/issues/54

--- servers/files/trident.textpattern.com/var/www/vhosts/textpattern.com/plugins/staging/build.sh
+++ servers/files/trident.textpattern.com/var/www/vhosts/textpattern.com/plugins/staging/build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 ### plugins.textpattern.com build script
 
-### get latest files
-rm -rf /var/www/vhosts/textpattern.com/plugins/staging/_github
-git clone --branch 'master' --depth 1 --single-branch https://github.com/textpattern/textpattern-curated-plugins-list.git /var/www/vhosts/textpattern.com/plugins/staging/_github
+### dirs are absolute and mUsT hAvE TrAiLiNg SlAsHeS oR ElSe
+repo_url="https://github.com/textpattern/textpattern-curated-plugins-list"
+repo_branch="master"
+repo_parent_dir="/var/www/vhosts/textpattern.com/plugins/staging/"
+repo_source_dir="/var/www/vhosts/textpattern.com/plugins/staging/_github/library-of-plugins/"
+web_target_dir="/var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/"
 
-### drop it (in) like it's hot
-if [ -f /var/www/vhosts/textpattern.com/plugins/staging/_github/README.md ]; then
-rm -rf /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/ \
-&& mkdir -p /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/ \
-&& mv /var/www/vhosts/textpattern.com/plugins/staging/_github/library-of-plugins/* /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/ \
-&& chown -R www-data:www-data /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/ \
-&& chmod -R 775 /var/www/vhosts/textpattern.com/plugins/live/library-of-plugins/
+rm -rf $repo_parent_dir/_github-$repo_branch
+git clone --branch '$repo_branch' --depth 1 --single-branch $repo_url.git $repo_parent_dir/_github-$repo_branch
+if [ -f $repo_parent_dir/_github-$repo_branch/README.md ]; then
+rm -rf $web_target_dir \
+&& mkdir -p $web_target_dir \
+&& mv $repo_source_dir* $web_target_dir \
+&& chown -R www-data:www-data $web_target_dir \
+&& chmod -R 775 $web_target_dir
 fi
