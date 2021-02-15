69f663e93c66216311ffb58d4e68fa123c9f8e67
Pete Cooper <pete@pragmatika.net>
2021-02-15 14:31:02 +0000

    start tracking plugins & themes build scripts
    they’re ugly, but they work
    
    paging https://github.com/textpattern/server-config/issues/54 for a reminder

---
+++ servers/files/tribune.textpattern.com/var/www/sites/textpattern.com/themes/staging/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+### themes.textpattern.com build script
+
+### dirs are absolute and mUsT NoT hAvE TrAiLiNg SlAsHeS oR ElSe
+repo_url="https://github.com/textpattern/textpattern-curated-themes-list"
+repo_branch="master"
+repo_parent_dir="/var/www/sites/textpattern.com/themes/staging"
+repo_source_dir="/var/www/sites/textpattern.com/themes/staging/_github-master/library-of-themes"
+web_target_dir="/var/www/sites/textpattern.com/themes/live/library-of-themes"
+
+rm -rf $repo_parent_dir/_github-$repo_branch
+git clone --branch "$repo_branch" --depth 1 --single-branch $repo_url.git $repo_parent_dir/_github-$repo_branch
+chown -R www-data:www-data $repo_parent_dir/_github-$repo_branch/
+chmod -R 775 $repo_parent_dir/_github-$repo_branch/
+if [ -f $repo_parent_dir/_github-$repo_branch/README.md ]; then
+rm -rf $web_target_dir/ \
+&& mkdir -p $web_target_dir/ \
+&& mv $repo_source_dir/* $web_target_dir/ \
+&& chown -R www-data:www-data $web_target_dir/ \
+&& chmod -R 775 $web_target_dir/
+fi
