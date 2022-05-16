92563fd666441d64983aafe26da08cf5aa6e7010
Pete Cooper <pete@pragmatika.net>
2022-05-16 12:47:43 +0100

    works, switch to main site

--- live/servers/files/tarzan.textpattern.net/var/www/servers/textpattern.com/docs/staging/mv-perms.sh
+++ live/servers/files/tarzan.textpattern.net/var/www/servers/textpattern.com/docs/staging/mv-perms.sh
@@ -4,9 +4,9 @@
 ### dirs are absolute and mUsT NoT hAvE TrAiLiNg SlAsHeS oR ElSe
 repo_url="https://github.com/textpattern/textpattern.github.io"
 repo_branch="self-hosted"
-repo_parent_dir="/var/www/servers/textpattern.com/docs-test/staging"
-repo_source_dir="/var/www/servers/textpattern.com/docs-test/staging/_github-self-hosted"
-web_target_dir="/var/www/servers/textpattern.com/docs-test/live/_site"
+repo_parent_dir="/var/www/servers/textpattern.com/docs/staging"
+repo_source_dir="/var/www/servers/textpattern.com/docs/staging/_github-self-hosted"
+web_target_dir="/var/www/servers/textpattern.com/docs/live/_site"
 
 if [ -f $repo_parent_dir/_github-$repo_branch/_site/index.html ]; then
 rm -rf $web_target_dir/ \
--- live/servers/files/tarzan.textpattern.net/var/www/servers/textpattern.com/docs/staging/site-build.sh
+++ live/servers/files/tarzan.textpattern.net/var/www/servers/textpattern.com/docs/staging/site-build.sh
@@ -4,9 +4,9 @@
 ### dirs are absolute and mUsT NoT hAvE TrAiLiNg SlAsHeS oR ElSe
 repo_url="https://github.com/textpattern/textpattern.github.io"
 repo_branch="self-hosted"
-repo_parent_dir="/var/www/servers/textpattern.com/docs-test/staging"
-repo_source_dir="/var/www/servers/textpattern.com/docs-test/staging/_github-self-hosted"
-web_target_dir="/var/www/servers/textpattern.com/docs-test/live/_site"
+repo_parent_dir="/var/www/servers/textpattern.com/docs/staging"
+repo_source_dir="/var/www/servers/textpattern.com/docs/staging/_github-self-hosted"
+web_target_dir="/var/www/servers/textpattern.com/docs/live/_site"
 
 rm -rf $repo_parent_dir/_github-$repo_branch \
 && git clone --branch "$repo_branch" --depth 1 --single-branch $repo_url.git $repo_parent_dir/_github-$repo_branch \
