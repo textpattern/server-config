6d51d99829670fba6c1a3e88619a4f83628670a7
Pete Cooper <pete@pragmatika.net>
2022-05-16 11:35:02 +0100

    try spinning out rbenv stuff to less frequent

---
+++ live/servers/files/tarzan.textpattern.net/var/www/servers/textpattern.com/docs/staging/rbenv-build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+### docs.textpattern.com site build script
+
+### dirs are absolute and mUsT NoT hAvE TrAiLiNg SlAsHeS oR ElSe
+repo_url="https://github.com/textpattern/textpattern.github.io"
+repo_branch="self-hosted"
+repo_parent_dir="/var/www/servers/textpattern.com/docs-test/staging"
+repo_source_dir="/var/www/servers/textpattern.com/docs-test/staging/_github-self-hosted"
+web_target_dir="/var/www/servers/textpattern.com/docs-test/live/_site"
+
+rm -rf $repo_parent_dir/_github-$repo_branch \
+&& git clone --branch "$repo_branch" --depth 1 --single-branch $repo_url.git $repo_parent_dir/_github-$repo_branch \
+&& cd $repo_parent_dir/_github-$repo_branch \
+&& ~/.rbenv/shims/bundle install \
+&& ~/.rbenv/shims/gem pristine --all \
+&& ~/.rbenv/shims/bundle exec jekyll build \
+&& if [ -f $repo_parent_dir/_github-$repo_branch/_site/index.html ]; then
+rm -rf $web_target_dir/ \
+&& mkdir -p $web_target_dir/ \
+&& mv $repo_source_dir/_site/* $web_target_dir/
+fi
