0aa2ff7caaeffe9c66d916a178587e3be77d8e88
Pete Cooper <pete@pragmatika.net>
2022-05-16 11:48:35 +0100

    split mv and permissions to own script

--- live/servers/files/tarzan.textpattern.net/var/www/servers/textpattern.com/docs/staging/mv-perms.sh
+++ live/servers/files/tarzan.textpattern.net/var/www/servers/textpattern.com/docs/staging/mv-perms.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-### docs.textpattern.com rbenv + _site build script
+### docs.textpattern.com site scaffold and permissions script
 
 ### dirs are absolute and mUsT NoT hAvE TrAiLiNg SlAsHeS oR ElSe
 repo_url="https://github.com/textpattern/textpattern.github.io"
@@ -8,9 +8,12 @@ repo_parent_dir="/var/www/servers/textpattern.com/docs-test/staging"
 repo_source_dir="/var/www/servers/textpattern.com/docs-test/staging/_github-self-hosted"
 web_target_dir="/var/www/servers/textpattern.com/docs-test/live/_site"
 
-rm -rf $repo_parent_dir/_github-$repo_branch \
-&& git clone --branch "$repo_branch" --depth 1 --single-branch $repo_url.git $repo_parent_dir/_github-$repo_branch \
-&& cd $repo_parent_dir/_github-$repo_branch \
-&& ~/.rbenv/shims/bundle install \
-&& ~/.rbenv/shims/gem pristine --all \
-&& ~/.rbenv/shims/bundle exec jekyll build
+if [ -f $repo_parent_dir/_github-$repo_branch/_site/index.html ]; then
+rm -rf $web_target_dir/ \
+&& echo "Moving files into place..." \
+&& mkdir -p $web_target_dir/ \
+&& mv $repo_source_dir/_site/* $web_target_dir/ \
+&& chmod -R www-data:www-data $web_target_dir/ \
+&& chown -R 775 $web_target_dir/
+&& echo "Done."
+fi
--- live/servers/files/tarzan.textpattern.net/var/www/servers/textpattern.com/docs/staging/site-build.sh
+++ live/servers/files/tarzan.textpattern.net/var/www/servers/textpattern.com/docs/staging/site-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-### docs.textpattern.com site build script
+### docs.textpattern.com _site build script
 
 ### dirs are absolute and mUsT NoT hAvE TrAiLiNg SlAsHeS oR ElSe
 repo_url="https://github.com/textpattern/textpattern.github.io"
@@ -12,10 +12,3 @@ rm -rf $repo_parent_dir/_github-$repo_branch \
 && git clone --branch "$repo_branch" --depth 1 --single-branch $repo_url.git $repo_parent_dir/_github-$repo_branch \
 && cd $repo_parent_dir/_github-$repo_branch \
 && ~/.rbenv/shims/bundle exec jekyll build
-if [ -f $repo_parent_dir/_github-$repo_branch/_site/index.html ]; then
-rm -rf $web_target_dir/ \
-&& echo "Moving files into place..." \
-&& mkdir -p $web_target_dir/ \
-&& mv $repo_source_dir/_site/* $web_target_dir/ \
-&& echo "Done."
-fi
