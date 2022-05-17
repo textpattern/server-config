1605c672098d8be9b94d287300ff50e20208db79
Pete Cooper <pete@pragmatika.net>
2022-05-17 08:16:27 +0100

    latest docs build tweaks

--- live/servers/files/tarzan.textpattern.net/var/www/servers/textpattern.com/docs/staging/site-build.sh
+++ live/servers/files/tarzan.textpattern.net/var/www/servers/textpattern.com/docs/staging/site-build.sh
@@ -11,4 +11,4 @@ web_target_dir="/var/www/servers/textpattern.com/docs/live/_site"
 rm -rf $repo_parent_dir/_github-$repo_branch \
 && git clone --branch "$repo_branch" --depth 1 --single-branch $repo_url.git $repo_parent_dir/_github-$repo_branch \
 && cd $repo_parent_dir/_github-$repo_branch \
-&& $HOME/.rbenv/shims/bundle exec jekyll build >> $repo_parent_dir/site-build.log
+&& $HOME/.rbenv/shims/bundle exec jekyll build
