e38546bf8499943690555c284c8a28d2d06fac1a
Pete Cooper <pete@pragmatika.net>
2024-05-10 10:21:47 +0100

    sifted and shifted

--- temp/_to check and purge/tarzan.textpattern.net/etc/nginx/scripts/h5bp-mime_types.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo mkdir -p /etc/nginx/includes/vendors-available/h5bp/
-sudo curl -Lo /etc/nginx/includes/vendors-available/h5bp/mime.types https://raw.githubusercontent.com/h5bp/server-configs-nginx/master/mime.types
-sudo chown -R root:sudo /etc/nginx/includes/vendors-available/h5bp/
-sudo chmod -R 775 /etc/nginx/includes/vendors-available/h5bp/
